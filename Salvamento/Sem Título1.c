c61cd829c4ef4c704eb4df5511f9adcc63f5b073
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-06-15 19:38:08 -0300

    Create Sem Título1.c

---
+++ Salvamento/Sem Título1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	printf("\t\t##################\n\t\t#\t\t #\n\t\t# AREA RESTRITA  #\n\t\t#\t\t #\n\t\t##################\n\n");
+	printf("\t\t##################\n\t\t#\t\t #\n\t\t# MENU DE ADM    #\n\t\t#\t\t #\n\t\t##################\n\n");
+	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# MENU DE FUNCIONARIO    #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
+	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# CADASTRO DE CLIENTE    #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
+	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# LOGIN DE CLIENTE    \t #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
+}
