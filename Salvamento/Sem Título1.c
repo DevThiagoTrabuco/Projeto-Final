fe8266f480a6ee1a627d769bd7451a8fd57c6680
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-06-15 19:42:59 -0300

    Update Sem Título1.c

--- Salvamento/Sem Título1.c
+++ Salvamento/Sem Título1.c
@@ -4,7 +4,10 @@
 int main(){
 	printf("\t\t##################\n\t\t#\t\t #\n\t\t# AREA RESTRITA  #\n\t\t#\t\t #\n\t\t##################\n\n");
 	printf("\t\t##################\n\t\t#\t\t #\n\t\t# MENU DE ADM    #\n\t\t#\t\t #\n\t\t##################\n\n");
+	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# CADASTRO DE ADM    \t #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
 	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# MENU DE FUNCIONARIO    #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
+	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# LOGIN DE FUNCIONARIO   #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
+	printf("\t\t###########################\n\t\t#\t\t\t  #\n\t\t# CADASTRO DE FUNCIONARIO #\n\t\t#\t\t\t  #\n\t\t###########################\n\n");
 	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# CADASTRO DE CLIENTE    #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
 	printf("\t\t##########################\n\t\t#\t\t\t #\n\t\t# LOGIN DE CLIENTE    \t #\n\t\t#\t\t\t #\n\t\t##########################\n\n");
 }
