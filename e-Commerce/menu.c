3bf46e6b103080adc7ee32696d3859548dcd6eb2
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-06-15 19:24:49 -0300

    Update menu.c

--- e-Commerce/menu.c
+++ e-Commerce/menu.c
@@ -10,6 +10,12 @@ void menu(){
 	setlocale(LC_ALL, "");
 		system("cls");
 		while(exit != 1){
+			if(logged==0){
+				printf("\nNão logado\n");
+			}
+			else{
+				printf("\nLogado\n");
+			}
 			printf("\t\t##################\n\t\t#\t\t #\n\t\t#      MENU      #\n\t\t#\t\t #\n\t\t##################\n\nBem vindo(a) à loja [PLACEHOLDER]!!");
 			printf("\n\n[1] - Login \n[2] - Cadastro\n[3] - Ir as compras\n[4] - Área restrita\n[5] - Fechar aplicação\n\nPor favor, escolha uma opção: ");
 			scanf("%c", &op);
@@ -18,17 +24,14 @@ void menu(){
 			switch(op){
 				case '1':
 					logged=loginClient();
-					
 					break;
 				case '2':
 					cadastrarClientes();
-					
 					break;
 				case '3':
 					if(logged==1){						
 						totalPrice = comprar();							
 						payment(totalPrice);
-						
 					}
 					else{
 						printf("\nLogue-se primeiro\n");
